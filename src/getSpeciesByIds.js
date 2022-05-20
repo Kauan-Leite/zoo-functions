3197b5fd19308cc93653c25abfa998e6dd9b2013
Kauan <kauan.s.leite@hotmail.com>
2022-05-20 16:23:20 -0300

    Lint do 1° Requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,20 +1,19 @@
-const { species } = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  let idA = ids[0];
-  let idB = ids[1];
+  const idA = ids[0];
+  const idB = ids[1];
 
-  if (idA === undefined){
+  if (idA === undefined) {
     return [];
-  } else {
-    const verifyID = () => (
-      data.species.filter((specie) => (specie.id === idA) || (specie.id === idB))
-    );
-    return verifyID();
   }
-}
 
+  const verifyID = () => (
+    data.species.filter((specie) => (specie.id === idA) || (specie.id === idB))
+  );
+  return verifyID();
+}
 
 module.exports = getSpeciesByIds;
