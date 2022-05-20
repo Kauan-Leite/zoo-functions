fa6b3b13130157fb859f0e288b4c783cd3eebe5d
Kauan <kauan.s.leite@hotmail.com>
2022-05-20 17:13:40 -0300

    3° Requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {}
+  }
+
+  return data.employees.find((nome) => (nome.firstName === employeeName) || (nome.lastName === employeeName), {});
+
 }
 
 module.exports = getEmployeeByName;
