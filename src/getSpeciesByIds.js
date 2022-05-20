b37d3f2894b2fcc34ca201204a963bd32bfaa882
Kauan <kauan.s.leite@hotmail.com>
2022-05-20 16:18:55 -0300

    1° Requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,20 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  let idA = ids[0];
+  let idB = ids[1];
+
+  if (idA === undefined){
+    return [];
+  } else {
+    const verifyID = () => (
+      data.species.filter((specie) => (specie.id === idA) || (specie.id === idB))
+    );
+    return verifyID();
+  }
 }
+
 
 module.exports = getSpeciesByIds;
