ca56c37475d59509bb16b3f033a714861b86e163
Kauan <kauan.s.leite@hotmail.com>
2022-05-23 18:07:06 -0300

    5° Requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const residenteName = data.species.map((resids) => resids.name);
+    const residenteQuant = data.species.map((resids) => resids.residents.length);
+    const obj = {};
+    for (let index = 0; index < residenteName.length; index += 1) {
+      obj[`${residenteName[index]}`] = parseFloat(`${residenteQuant[index]}`);
+    } return obj;
+  }
+  const searchName = data.species.find((nome) => nome.name === animal.specie);
+
+  if (animal.specie !== undefined && animal.sex === undefined) {
+    return searchName.residents.length;
+  }
+  const residenteSex = animal.sex;
+  const searchSex = searchName.residents.filter((sex) => sex.sex === residenteSex).length;
+  return searchSex;
 }
 
 module.exports = countAnimals;
