4776369e7edff70dba86a5cbf72c6241e39a5997
Kauan <kauan.s.leite@hotmail.com>
2022-05-25 15:27:46 -0300

    10° Requisito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,22 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const searchId = data.employees.filter((element) => element.id === id);
+  const management = searchId.map((element) => element.responsibleFor[0])[0];
+
+  const animal = data.species.filter((element) => element.id === management);
+  const fillResids = animal.map((element) => element.residents)[0];
+
+  let mostAge = 0;
+  let indexToArr = 0;
+  for (let index = 0; index < fillResids.length; index += 1) {
+    if (fillResids[index].age > mostAge) {
+      mostAge = fillResids[index].age;
+      indexToArr = index;
+    }
+  }
+  const msg = [fillResids[indexToArr].name, fillResids[indexToArr].sex, fillResids[indexToArr].age];
+  return msg;
 }
 
 module.exports = getOldestFromFirstSpecies;
